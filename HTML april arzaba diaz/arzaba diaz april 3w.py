9da45e7421c8a30c147fb1d8176d855c79a088f1
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-11-06 16:21:19 -0600

    Add files via upload

---
+++ HTML april arzaba diaz/arzaba diaz april 3w.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My primer encabezdo</h1>
+<h2>My primer encabezdo</h2>
+<h3>My primer encabezdo</h3>
+<h4>My primer encabezdo</h4>
+<h5>My primer encabezdo</h5>
+<p>My primer parrafo.</p>
+
+</body>
+</html>
